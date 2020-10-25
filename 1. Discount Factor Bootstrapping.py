37db103687ab08cbc369478f92bd3942286888a5
Yunfeng <yzhao20@yahoo.com>
2020-10-25 18:25:22 +1100

    Create 1. Discount Factor Bootstrapping.py

---
+++ 1. Discount Factor Bootstrapping.py
@@ -0,0 +1,5 @@
+"""
+The daily BBSW market yield has a dozens of term points, but the calculation of market value of a banking book requires monthly discount factors up to 30 years (360 months). 
+So, the first step is uploading the market yield and generate the daily monthly discount factors.
+"""
+
