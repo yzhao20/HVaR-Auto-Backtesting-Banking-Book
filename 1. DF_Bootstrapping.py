97488bde8e91e0749be2fd5befd0a7bbb2aa259c
Yunfeng <yzhao20@yahoo.com>
2020-10-29 09:35:31 +1100

    Update 1. DF_Bootstrapping.py

--- 1. DF_Bootstrapping.py
+++ 1. DF_Bootstrapping.py
@@ -1,5 +1,8 @@
 """
 The daily BBSW market yield has a dozens of term points, but the calculation of market value of a banking book requires monthly discount factors up to 30 years (360 months). 
 So, the first step is uploading the market yield and generate the daily monthly discount factors.
+
+Input: Market Yield Curve (BBSW + Swap Markets Rate)
+Output: 
 """
 
